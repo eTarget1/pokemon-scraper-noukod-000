f4d78b42932f19984db24daf482f49f8f03baa17
clifford <clifford@noukod.com>
2018-11-22 18:18:45 +0000

    automatically backed up by learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,9 +1,9 @@
 # class Pokemon
-# 
+#
 #   @@all = []
-# 
+#
 #    attr_accessor :id, :name, :type, :db, :hp
-# 
+#
 #   def initialize(id: nil, name:, type: , db:, hp: 60)
 #       @id = id
 #       @name = name
@@ -11,21 +11,21 @@
 #       @db = db
 #       @@all << self
 #     end
-# 
+#
 #     def self.save(name, type, db)
 #    db.execute("INSERT INTO pokemon(name, type) VALUES(?, ?)", [name, type])
-# 
+#
 #  end
-# 
+#
 #  def self.save(name, type, db)
 #  data = db.prepare("INSERT INTO pokemon (name, type) VALUES (?,?)")
 #  data.execute(name,type)
 # end
-# 
+#
 # def self.find(id, db)
 #  data = db.prepare("SELECT * FROM pokemon WHERE id = ?")
 #  result_set = data.execute(id)
-# 
+#
 #  results = result_set.collect do |row|
 #    pokemon = Pokemon.new(name: row[1], type: row[2], db: db, id: id)
 #    pokemon.hp = row[3]
@@ -40,7 +40,7 @@
 #  #    # db.execute("UPDATE pokemon SET hp = ? WHERE id = ?",60,id_) if result.length != 3
 #  #    new(id: id_, name: name, type: type,db: db, hp: hp)
 #  #    end
-# 
+#
 #  def alter_hp(new_val, db)
 #      db.execute("UPDATE pokemon SET hp = ? WHERE id = ?", [new_val, self.id])
 #   end
